faf2e04b68251f05e43cd582d340309fb2d56daa
kariebi <sorohmichael@gmail.com>
2023-11-15 18:22:26 -0500

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import Link from 'next/link'
+import { Empty } from "@/components/ui/empty";
+
+export default function NotFound() {
+  return (
+    <Empty label="Sorry Page Not Found." />
+  )
+}
